8c8dde621ceaa7e61ab38a7c8fbcd03c1967038a
Sebastian Mendez <sebas.mendezg@gmail.com>
2025-04-21 14:38:04 -0500

    feat: metodo create controlador de post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,22 @@ class PostsController < ApplicationController
 
     redirect_to :root if @post.nil?
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.expect(post: [ :title, :description ])
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,4 +18,12 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to redirect_to(:root)
     end
   end
+  describe "create" do
+    it "creates a new post when the required params are given" do
+        expect { post :create, params: { post: { title: 'Hello world', description: 'Mi descripción' } } }.to change(Post, :count).by(1)
+    end
+    it "does not create a new post when one param is missing" do
+        expect { post :create, params: { post: { title: 'Hello world', description: nil } } }.to change(Post, :count).by(0)
+    end
+  end
 end
