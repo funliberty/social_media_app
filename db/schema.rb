90402b08d61ed07ceae85175bba6ecba82fedd89
EstivenB01 <gonxalod36@gmail.com>
2025-05-15 22:06:38 +0000

    actualizacion de html para tailwind

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_04_005115) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "media_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema[8.0].define(version: 0) do
 end
